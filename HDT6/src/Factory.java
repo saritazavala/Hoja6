ac0036bad9a9e0339318fddb6e3add6c3b8437e1
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-03-02 23:05:51 -0600

    First
    Factory and main created

--- HDT6/src/Factory.java
+++ HDT6/src/Factory.java
@@ -1,3 +1,31 @@
-public class Factory {
+/*Sara Zavala 18893
+Hoja de Trabajo 6
+03 de Marzo de 2019
+Estructura de Datos
+ */
+import java.io.*;
+import java.util.*;
+import java.util.Collections.*;
+
+/*
+Refenrencias
+Tutorialspoint. Design Pattern - Factory Pattern.
+https://www.tutorialspoint.com/design_pattern/factory_pattern.htm
+ */
+public class Factory<k,v> {
+    public Map getMap(String tipoMapa){
+
+        if (tipoMapa== null){
+            return  null;
+        }
+
+        if (tipoMapa.equalsIgnoreCase("Hashmap")){
+            return  new HashMap<>();
+        }
+
+        return null;
+    }
+
+
 
 }
---
+++ HDT6/src/Main.java
@@ -0,0 +1,17 @@
+/*Sara Zavala 18893
+Hoja de Trabajo 6
+03 de Marzo de 2019
+Estructura de Datos
+ */
+
+
+import java.util.HashMap;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("prueba");
+    }
+
+}
